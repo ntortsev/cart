35cc54112cd60afdb1dd49a5844b610052d39d4b
Nikita Tortsev <93070963+ntortsev@users.noreply.github.com>
2023-04-05 11:40:06 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+//импортирование компонента Cart
+import { Cart } from "./components";
+
+//компонент App
+function App() {
+  return (
+    <div className="container">
+      {/* компонент Cart */}
+      <Cart />
+    </div>
+  );
+}
+
+export default App;
